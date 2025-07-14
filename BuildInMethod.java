937eaa4574e6dcac7499b47c5d9c2f7236a0ee4c
Rahul Kumar Gupta <114803919+rkgupta8858@users.noreply.github.com>
2025-07-14 16:08:18 +0530

    Add files via upload

---
+++ BuildInMethod.java
@@ -0,0 +1,11 @@
+class BuildInMethod
+{
+	public static void main(String args[])
+	{
+		System.out.println("This is a build in method !!!");
+		
+		int a = 1000;
+		System.out.println(Math.random());
+	
+	}
+}
