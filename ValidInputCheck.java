8da2317a252b1709c2b9ed2f605bcce567884737
Rahul Kumar Gupta <114803919+rkgupta8858@users.noreply.github.com>
2025-07-15 14:57:29 +0530

    Add files via upload

---
+++ ValidInputCheck.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class ValidInputCheck
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Anything : ");
+		String input = sc.nextLine();
+		
+		if(input==null || input.trim().isEmpty())
+		{
+			System.out.print("False");
+		}
+		else
+		{
+			System.out.print("True");
+		}
+	}
+}
