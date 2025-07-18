f440adbc195fd1544d97abe861e3e81594d93ac3
Rahul Kumar Gupta <114803919+rkgupta8858@users.noreply.github.com>
2025-07-18 13:33:12 +0530

    Add files via upload

---
+++ Week 1/Fri/PersonalDetails.java
@@ -0,0 +1,15 @@
+class AadharCardData
+{
+	public static void main(String args[])
+	{
+		System.out.println("Aadhar card Number : 4564566");
+	}
+}
+
+class PanCardData
+{
+	public static void main(String args[])
+	{
+		System.out.println("Pan card Number : DKCPG6153F");
+	}
+}
