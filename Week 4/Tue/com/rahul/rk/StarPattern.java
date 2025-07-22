4e1293f50375d0c9f3e5ee961f90b5a6c4ccce33
Rahul Kumar Gupta <114803919+rkgupta8858@users.noreply.github.com>
2025-07-22 12:38:41 +0530

    Add files via upload

---
+++ Week 4/Tue/com/rahul/rk/StarPattern.java
@@ -0,0 +1,17 @@
+package com.rahul.rk;
+import java.util.Scanner;
+
+class StarPattern
+{
+	public static void main(String args[])
+	{
+		for(int i=1; i<=5; i++)
+		{
+			for(int j=1;j<=i; j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
