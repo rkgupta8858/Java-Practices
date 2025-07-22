e779fb99ff639dd52007d29c399ea5e0f9a4f60a
Rahul Kumar Gupta <114803919+rkgupta8858@users.noreply.github.com>
2025-07-22 12:47:46 +0530

    Add files via upload

---
+++ Week 4/Tue/com/rahul/rk/StarPatternOpposite.java
@@ -0,0 +1,24 @@
+/*
+	*****
+	****
+	***
+	**
+	*	
+*/
+package com.rahul.rk;
+import java.util.Scanner;
+
+class StarPatternOpposite
+{
+	public static void main(String args[])
+	{
+		for(int i=1; i<=5; i++)
+		{
+			for(int j=5; j>=i; j--)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
