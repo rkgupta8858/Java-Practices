b505d997ccb91974908e795c77c0e49543a46127
Rahul Kumar Gupta <114803919+rkgupta8858@users.noreply.github.com>
2025-07-26 23:24:08 +0530

    Add files via upload

---
+++ Week 4/Fri/Sub3.java
@@ -0,0 +1,11 @@
+// method with parameter with return type
+public class Sub3 {
+    static int subNumber(int num1, int num2){
+        int result = num1 - num2;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(subNumber(20, 30));
+    }
+}
