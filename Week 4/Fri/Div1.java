8cadaf7326271841fcc8638a8aeba48c63c2c6d1
Rahul Kumar Gupta <114803919+rkgupta8858@users.noreply.github.com>
2025-07-26 23:23:37 +0530

    Add files via upload

---
+++ Week 4/Fri/Div1.java
@@ -0,0 +1,12 @@
+// method without parameter without return type
+public class Div1 {
+    static void divNumber(){
+        int num1 = 100;
+        int num2 = 20;
+        int result = num1 / num2;
+        System.out.println(result);
+    }
+    public static void main(String[] args) {
+        divNumber();
+    }
+}
