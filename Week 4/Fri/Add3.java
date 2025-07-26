5f2080d6ece351ddf3abb79b2231ed7086000249
Rahul Kumar Gupta <114803919+rkgupta8858@users.noreply.github.com>
2025-07-26 23:23:04 +0530

    Add files via upload

---
+++ Week 4/Fri/Add3.java
@@ -0,0 +1,11 @@
+// method with parameter with return type
+public class Add3 {
+    static int addNumber(int num1, int num2){
+        int result = num1 + num2;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(addNumber(20, 30));
+    }
+}
