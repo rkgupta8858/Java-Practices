5d45f0f06e7f8b4b59c9f1dac500431f366ea121
Rahul Kumar Gupta <114803919+rkgupta8858@users.noreply.github.com>
2025-07-26 23:22:37 +0530

    Add files via upload

---
+++ Week 4/Fri/Add2.java
@@ -0,0 +1,11 @@
+// method with parameter without return type
+public class Add2 {
+    static void addNumber(int num1, int num2){
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        addNumber(10, 20);
+    }
+}
