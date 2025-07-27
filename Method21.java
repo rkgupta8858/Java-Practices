4ef9670b92156f82c9a69a40099ef8b31a2549bf
Rahul Kumar Gupta <114803919+rkgupta8858@users.noreply.github.com>
2025-07-27 20:11:31 +0530

    Add files via upload

---
+++ Method21.java
@@ -0,0 +1,11 @@
+// method without parameter without return type
+public class Method21 {
+    static void doubleNumber(){
+        int num = 10;
+        int result = num * 2;
+        System.out.println(result);
+    }
+    public static void main(String[] args) {
+        doubleNumber();
+    }
+}
