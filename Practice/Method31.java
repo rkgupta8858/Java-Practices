714dcbfd9b197193874bd0b8fe2c53b30fc6216a
Rahul Kumar Gupta <114803919+rkgupta8858@users.noreply.github.com>
2025-07-27 20:10:26 +0530

    Add files via upload

---
+++ Practice/Method31.java
@@ -0,0 +1,10 @@
+// method with parameter without return type
+public class Method31 {
+    static void doubleNumber(int num){
+        int result = num * 2;
+        System.out.println(result);
+    }
+    public static void main(String[] args) {
+        doubleNumber(10);
+    }
+}
