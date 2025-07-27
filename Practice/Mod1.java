8cfcdaf4f636d53b367af5e24f2ffa4d4bac1ffb
Rahul Kumar Gupta <114803919+rkgupta8858@users.noreply.github.com>
2025-07-27 20:06:55 +0530

    Add files via upload

---
+++ Practice/Mod1.java
@@ -0,0 +1,12 @@
+// method without parameter without return type
+public class Mod1 {
+    static void modNumber(){
+        int num1 = 25;
+        int num2 = 20;
+        int result = num1 % num2;
+        System.out.println(result);
+    }
+    public static void main(String[] args) {
+        modNumber();
+    }
+}
