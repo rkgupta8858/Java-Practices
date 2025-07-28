778aabe3d5f26ad3b012791940150283329cbfaa
Rahul Kumar Gupta <114803919+rkgupta8858@users.noreply.github.com>
2025-07-28 13:59:02 +0530

    Add files via upload

---
+++ Array2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.Arrays;
+class Array2
+{
+	public static void main(String args[])
+	{
+		int[] arr = new int[5];
+		Scanner sc = new Scanner(System.in);
+		
+		for(int i=0; i<arr.length; i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		System.out.println(Arrays.toString(arr));
+	}
+}
