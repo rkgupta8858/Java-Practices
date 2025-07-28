996b51a61a1629dd710ed2f3a28fefa4c618d4b9
Rahul Kumar Gupta <114803919+rkgupta8858@users.noreply.github.com>
2025-07-28 14:00:48 +0530

    Add files via upload

---
+++ Week 5/Mon/Array1.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Array1
+{
+	public static void main(String args[])
+	{
+		double[] arr = new double[6];
+		arr[0]=10;
+		arr[1]=20;
+		arr[2]=30;
+		arr[3]=60;
+		arr[4]=50;
+		arr[5]=80;
+		
+		System.out.println(Arrays.toString(arr));
+		
+	}
+}
