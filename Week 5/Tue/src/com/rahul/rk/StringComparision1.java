7c7559e134563c06b92be97fa6d1fecbb5d21b86
Rahul Kumar Gupta <114803919+rkgupta8858@users.noreply.github.com>
2025-07-29 18:07:11 +0530

    Add files via upload

---
+++ Week 5/Tue/src/com/rahul/rk/StringComparision1.java
@@ -0,0 +1,17 @@
+package com.rahul.rk;
+
+public class StringComparision1 {
+
+	public static void main(String[] args) {
+		String s1 = new String("Rahul");
+		String s2 = new String("Rahul");
+		if(s1.equals(s2))
+		{
+			System.out.println("True");
+		}
+		else
+		{
+			System.out.println("False");
+		}
+	}
+}
