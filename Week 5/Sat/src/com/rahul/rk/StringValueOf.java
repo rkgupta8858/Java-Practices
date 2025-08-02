73d1b1e3919a945ad08b9b67956b44c2f816a3c6
Rahul Kumar Gupta <114803919+rkgupta8858@users.noreply.github.com>
2025-08-02 19:50:57 +0530

    Add files via upload

---
+++ Week 5/Sat/src/com/rahul/rk/StringValueOf.java
@@ -0,0 +1,21 @@
+package com.rahul.rk;
+
+public class StringValueOf {
+
+	public static void main(String[] args) {
+		String s1 = "20";
+		String s2 = "25";
+		
+		int a=10;
+		int b=20;
+		
+		System.out.println(s1+s2);
+		
+		System.out.println(String.valueOf(s1)+String.valueOf(s2));
+		System.out.println(String.valueOf(a)+String.valueOf(b));
+		System.out.println(a+b);
+		
+
+	}
+
+}
