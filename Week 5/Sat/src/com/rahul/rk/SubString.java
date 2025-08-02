991ab3f943dd880c3a190aa06d79f723968ae26c
Rahul Kumar Gupta <114803919+rkgupta8858@users.noreply.github.com>
2025-08-02 19:57:34 +0530

    Add files via upload

---
+++ Week 5/Sat/src/com/rahul/rk/SubString.java
@@ -0,0 +1,12 @@
+package com.rahul.rk;
+
+public class SubString {
+
+	public static void main(String[] args) {
+		String str = "Rahul";
+		System.out.println(str.substring(1,4));
+		System.out.println(str.substring(2,3));
+
+	}
+
+}
