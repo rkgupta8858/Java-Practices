7c353929c6dcc64e23ec5544e40e72c525fe9ddc
Rahul Kumar Gupta <114803919+rkgupta8858@users.noreply.github.com>
2025-08-05 13:50:44 +0530

    Add files via upload

---
+++ Week 6/Tue/src/com/rahul/replace/ReplaceMrthod.java
@@ -0,0 +1,16 @@
+package com.rahul.replace;
+
+public class ReplaceMrthod {
+
+	public static void main(String[] args) {
+		String s = "Rahul Kumar Gupta";
+		
+		s =s.replace(" ", "%");
+		System.out.println(s);
+		
+		s =s.replace("a", "@");
+		System.out.println(s);
+
+	}
+
+}
