7820be0e49159f8b2d337588bd297417da6c69e1
Rahul Kumar Gupta <114803919+rkgupta8858@users.noreply.github.com>
2025-08-06 12:00:49 +0530

    Add files via upload

---
+++ Week 6/Wed_OOPs/src/com/rahul/oops/EncapsulationSettetGetter.java
@@ -0,0 +1,10 @@
+package com.rahul.oops;
+
+public class EncapsulationSettetGetter {
+
+	private int productId;
+	private String productName;
+	private double productPrice;
+	private String address;
+
+}
---
+++ Week 6/Wed_OOPs/src/com/rahul/oops/Orders.java
@@ -0,0 +1,26 @@
+package com.rahul.oops;
+
+public class Orders {
+
+	private String orderStatus;
+	private String orderAddess;
+	private int orderAmount;
+	public String getOrderStatus() {
+		return orderStatus;
+	}
+	public void setOrderStatus(String orderStatus) {
+		this.orderStatus = orderStatus;
+	}
+	public String getOrderAddess() {
+		return orderAddess;
+	}
+	public void setOrderAddess(String orderAddess) {
+		this.orderAddess = orderAddess;
+	}
+	public int getOrderAmount() {
+		return orderAmount;
+	}
+	public void setOrderAmount(int orderAmount) {
+		this.orderAmount = orderAmount;
+	}
+}
