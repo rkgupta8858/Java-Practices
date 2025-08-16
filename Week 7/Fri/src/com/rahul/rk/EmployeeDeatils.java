d4f99b2493de5e23bbb60204210bfeccdcd9f029
Rahul Kumar Gupta <114803919+rkgupta8858@users.noreply.github.com>
2025-08-16 20:09:13 +0530

    Add files via upload

---
+++ Week 7/Fri/src/com/rahul/rk/EmployeeDeatils.java
@@ -0,0 +1,37 @@
+package com.rahul.chaining;
+
+class Employee {
+	int id;
+	String name;
+
+	public Employee() {
+		this(101, "Rahul");
+	}
+
+	Employee(int _id, String _name) {
+		this.name = _name;
+		this.id = _id;
+		System.out.println(id + " " + name);
+	}
+}
+
+class Manager extends Employee {
+	String department;
+
+	public Manager() {
+		this("Computer Science");
+	}
+
+	Manager(String _department) {
+		super();
+		this.department = _department;
+		System.out.println(department);
+	}
+
+}
+
+public class EmployeeDeatils {
+	public static void main(String[] args) {
+		Manager manager = new Manager();
+	}
+}
