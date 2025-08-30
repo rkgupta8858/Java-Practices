b9a45c50a0a2e36d46df888c94921d360ff2e33f
Rahul Kumar Gupta <114803919+rkgupta8858@users.noreply.github.com>
2025-08-30 13:04:18 +0530

    Add files via upload

---
+++ Week 9/StaticAndInstance/src/com/rahul/staticblock/Test.java
@@ -0,0 +1,12 @@
+package com.rahul.staticblock;
+
+public class Test {
+	// form Java 1.7 version main method is mandatory to execute static block
+	static {
+		System.out.println("Hello This is Satatic block !!");
+		System.exit(0);
+	}
+	public static void main(String[] args) {
+		
+	}
+}
