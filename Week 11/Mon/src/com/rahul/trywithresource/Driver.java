557227726126bb522287b59d82a0a52476288a08
Rahul Kumar Gupta <114803919+rkgupta8858@users.noreply.github.com>
2025-09-08 17:56:09 +0530

    Add files via upload

---
+++ Week 11/Mon/src/com/rahul/trywithresource/Driver.java
@@ -0,0 +1,15 @@
+package com.rahul.trywithresource;
+
+import java.io.BufferedReader;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void readFiles() {
+		BufferedReader br = new BufferedReader(null);
+	}
+
+}
