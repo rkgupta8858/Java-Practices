5f1d71bdd4e0a61bae3b497525ba485dd50131b3
Rahul Kumar Gupta <114803919+rkgupta8858@users.noreply.github.com>
2025-09-08 18:11:35 +0530

    Add files via upload

--- Week 11/Mon/src/com/rahul/trywithresource/Driver.java
+++ Week 11/Mon/src/com/rahul/trywithresource/Driver.java
@@ -1,15 +1,26 @@
 package com.rahul.trywithresource;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Driver {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		Driver d = new Driver();
+		d.readFiles();
 		
 	}
-	public void readFiles() {
-		BufferedReader br = new BufferedReader(null);
+	public void readFiles() throws FileNotFoundException, IOException {
+		try(BufferedReader br = new BufferedReader(new FileReader(""))){
+			String line = br.readLine();
+		    System.out.println(" reading file");
+		}
+		catch(Exception e){
+			System.out.println("File not found !!");
+		}
+		
 	}
 
 }
