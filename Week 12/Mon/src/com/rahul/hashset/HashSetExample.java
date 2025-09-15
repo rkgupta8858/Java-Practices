3f130828901d6af0e1e9fae1c211d6f675f0c437
Rahul Kumar Gupta <114803919+rkgupta8858@users.noreply.github.com>
2025-09-15 10:36:10 +0530

    Add files via upload

---
+++ Week 12/Mon/src/com/rahul/hashset/HashSetExample.java
@@ -0,0 +1,26 @@
+package com.rahul.hashset;
+
+import java.util.HashSet;
+
+public class HashSetExample {
+
+	public static void main(String[] args) {
+		HashSet<String> products = new HashSet<String>();
+		products.add("Apple");
+		products.add("Samsung");
+		products.add("Oppo");
+		products.add("Vivo");
+		products.add("Fan");
+		products.add("AC");
+		products.add("Cooler");
+		products.add("Bulb");
+		products.add("Laptop");
+		products.add("Study Lamp");
+		products.add("USB Light");
+		products.add("Charger");
+		products.add("Charger1");
+		
+
+		System.out.println(products);
+	}
+}
