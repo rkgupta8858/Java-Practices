78a52ec303683f121d35786c8a2392e5c6a11eb8
Rahul Kumar Gupta <114803919+rkgupta8858@users.noreply.github.com>
2025-09-22 19:56:10 +0530

    Add files via upload

--- Practice/Driver.java
+++ Practice/Driver.java
@@ -1,10 +1,12 @@
 package com.rahul.treeset;
 
+import java.util.Iterator;
 import java.util.TreeSet;
 
 public class Driver {
 
 	public static void main(String[] args) {
+		int i = 1;
 		TreeSet<String> treeSet = new TreeSet<String>();
 		treeSet.add("Mango");
 		treeSet.add("Banana");
@@ -13,6 +15,12 @@ public class Driver {
 		treeSet.add("Mango");
 		
 		System.out.println(treeSet);
+		
+		Iterator<String> itr = treeSet.iterator();
+		while(itr.hasNext()) {
+			System.out.println(i +"-"+itr.next());
+			i++;
+		}
 
 	}
 
