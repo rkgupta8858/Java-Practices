91f923e80e21d16a55be46ea13e48dfc6daa42b5
Rahul Kumar Gupta <114803919+rkgupta8858@users.noreply.github.com>
2025-10-18 22:15:13 +0530

    Add files via upload

---
+++ Practice/SwapTwoNumber.java
@@ -0,0 +1,12 @@
+class SwapTwoNumber
+{	
+	public static void main(String args[])
+	{
+		int a = 10;
+		int b = 20;
+		a = a+b;
+		b = a-b;
+		a = a-b;
+		System.out.println("a is : " + a + "\nb is : "+ b);
+	}
+}
