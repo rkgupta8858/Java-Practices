242a11cb0447161cbec0ce0c5083294e51fd4725
Rahul Kumar Gupta <114803919+rkgupta8858@users.noreply.github.com>
2025-10-18 22:13:49 +0530

    Add files via upload

---
+++ Practice/DataType.java
@@ -0,0 +1,18 @@
+class DataType
+{
+	public static void main(String args[])
+	{
+		byte b = 100;
+		System.out.println("Byte Value is : " +b);
+		
+		char c = 'C';
+		System.out.println("Character value is : " +c);
+		
+		int i = 5456625;
+		System.out.println("Integer value is : "+i);
+		
+		short s = 1111;
+		System.out.println("Short value is : "+s);
+	}
+	
+}
