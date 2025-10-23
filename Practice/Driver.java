199a0bf1569b428451ec71a6ed52ffb5518e1e48
Rahul Kumar Gupta <114803919+rkgupta8858@users.noreply.github.com>
2025-10-23 17:03:30 +0530

    Add files via upload

--- Practice/Driver.java
+++ Practice/Driver.java
@@ -1,35 +1,26 @@
-package com.rahul.callable1;
+package com.rahul.executorexp;
 
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-class MyThread implements Callable<String>
-{
-
+class MyThread implements Runnable{
 	@Override
-	public String call() {
-		System.out.println("MyThread.call()"+ Thread.currentThread().getName());
-		String cars =car();
-		return cars;
-		
+	public void run() {
+		System.out.println("MyThread.run()"+Thread.currentThread().getName());
 	}
-	public String car() {
-		System.out.println("MyThread.car()"+Thread.currentThread().getName());
-		return null;
-	}
-	
 }
+
+
 
 public class Driver {
 
 	public static void main(String[] args) {
-		MyThread myThread = new MyThread();
 		ExecutorService executorService = Executors.newCachedThreadPool();
+		MyThread  myThread = new MyThread();
+		Thread thread = new Thread(myThread);
 		for (int i = 0; i < 5; i++) {
-			executorService.submit(myThread);
+			executorService.execute(thread);
 		}
-		executorService.shutdown();
 	}
 
 }
