6fff1c60d29fba1bc4bb97b17bee8fd04e2cd02a
Rahul Kumar Gupta <114803919+rkgupta8858@users.noreply.github.com>
2025-11-23 23:20:07 +0530

    Add files via upload

---
+++ Practice/treeify/DriverHashSet.java
@@ -0,0 +1,36 @@
+package com.rahul.treeify;
+
+import java.util.HashSet;
+
+class Student{
+	String name;
+	public Student(String name) {
+		this.name=name;
+	}
+	
+	@Override
+	public int hashCode() {
+		return 12345;
+	}
+}
+
+public class DriverHashSet {
+
+	public static void main(String[] args) {
+		HashSet<Student> hashSet = new HashSet<Student>(64);
+
+		hashSet.add(new Student("Rahul"));
+		hashSet.add(new Student("Rohit"));
+		hashSet.add(new Student("Ravi"));
+		hashSet.add(new Student("Rohan"));
+		hashSet.add(new Student("Anand"));
+		hashSet.add(new Student("Aryan"));
+		hashSet.add(new Student("Ash"));
+		hashSet.add(new Student("Ashu"));
+		hashSet.add(new Student("Ashutosh"));
+		hashSet.add(new Student("Ashu s"));
+		
+		System.out.println(hashSet);
+	}
+
+}
