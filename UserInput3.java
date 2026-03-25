4a3961c2a7427d88bf5824ff0b69a9ed21740f44
Rahul Kumar Gupta <114803919+rkgupta8858@users.noreply.github.com>
2026-03-25 11:30:56 +0530

    Update UserInput3.java

--- UserInput3.java
+++ UserInput3.java
@@ -5,7 +5,7 @@ class UserInput3
 	public static void main(String args[])
 	{
 		Scanner scanner = new Scanner(System.in);
-		
+		 
 		System.out.print("Enter your name : ");
 		String name = scanner.next(); 
 		System.out.println("Name is : "+ name);
